bd1039ac75948ec065e3c4034f80789fe19c8570
tgb77779999-frank <tgb77779999@gmail.com>
2026-05-21 20:09:29 +0800

    Fix local start command

--- start-local.cmd
+++ start-local.cmd
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 echo Starting local server...
 echo.
-"C:\Program Files\nodejs\npm.cmd" run start -- --hostname 127.0.0.1 --port 3000
+"C:\Program Files\nodejs\npm.cmd" run dev -- --hostname 127.0.0.1 --port 3000
 echo.
 echo Server stopped. Press any key to close this window.
 pause > nul
